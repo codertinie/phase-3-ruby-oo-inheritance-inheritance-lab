50ddf6b4cbc90acc1bfac8966778e6d2eb8cd37c
codertinie <martin.munyao@moringaschool.com>
2022-10-25 15:34:31 +0300

    Completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+require 'pry'
 
+class Student < User
+  
+  attr_accessor :knowledge, :learn
+  
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
 end
+binding.pry
